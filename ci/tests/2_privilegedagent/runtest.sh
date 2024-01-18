bb4d5c8f648c3c0fb382838f2718d2c4e261d663
George Almasi <gheorghe@us.ibm.com>
2024-01-18 15:17:53 +0000

    CI: typo fixes in scripting system. Also, original implementation was way too dependent on what directory it got invoked from; attempting to make it more resilient.
    Signed-off-by: George Almasi <gheorghe@us.ibm.com>

--- ci/tests/2_privilegedagent/runtest.sh
+++ ci/tests/2_privilegedagent/runtest.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+bindir=$(dirname $(realpath $0))
+cd ${bindir}/../../..
+
 . ci/util/util_ao.sh
 
 ao_build      . || exit -1
 ao_clean      . || exit -1
-ao_deploy     . ${PWD}/ci/tests/2_privilegedagent/values.yml || exit -1
+ao_deploy     . ${bindir}/values.yml || exit -1
 ao_wait       . || exit -1
 ao_simpletest . || exit -1
 ao_clean      .
